8189268739117b3e44de880c00258b707b658695
Muhmmad Awd <muhmmadawd@gmail.com>
2022-10-27 14:49:17 +0300

    creating-Database-at-startup
    #5

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Core.Entities
 {
     public class Product : BaseEntity
     {
         public string Desc { get; set; }
         public decimal Price { get; set; }
+        [Required]
         public string PictureUrl { get; set; }
 
         public ProductType productType { get; set; }
